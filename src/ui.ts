2b4727be3d4c8cd8ff32b90749619233d5c64a14
stagfoo <yo@stagfoo.com>
2023-10-10 19:14:55 +0900

    🐛 add fixed url img

--- src/ui.ts
+++ src/ui.ts
@@ -14,9 +14,9 @@ export function playerHandler(state) {
   return html`
   <div id="cover-uploader">
     <input type="file" id="img" name="img" accept="image/*;capture=camera" onchange=${(e) => actions.playTrack(e)} >
-    <img id="cover-area" src="/assets/empty-case.png"/>
+    <img id="cover-area" src="https://mini-player.cover.pages.dev/assets/empty-case.png"/>
     <div id="track-cover" style="background-image:url(${state.trackCover})" ></div>
-    <img id="cover-area-cd" class="${state.track !== "" ? "spin": ""}" src="${state.track !== "" ? "/assets/cd.png": ""}"/>
+    <img id="cover-area-cd" class="${state.track !== "" ? "spin": ""}" src="${state.track !== "" ? "https://mini-player.cover.pages.dev/assets/cd.png": ""}"/>
     <iframe width="1" height="1" src="https://www.youtube.com/embed/${state.track}/?autoplay=1&enablejsapi=1&origin=${window.location.href}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
   `
