5c78310562987d5738a7ca16db3ccf16bbe08d6d
stagfoo <yo@stagfoo.com>
2023-10-11 15:50:05 +0900

    🐛 remove webworker code

--- src/index.ts
+++ src/index.ts
@@ -41,16 +41,3 @@ function renderer(newState) {
 //Start Router listener
 startRouters();
 BaseStyles();
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./service-worker.js').then(
-          registration => {
-              console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
-          },
-          error => {
-              console.log(`ServiceWorker registration failed: ${error}`);
-          }
-      );
-  });
-}
